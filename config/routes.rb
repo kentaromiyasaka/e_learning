539153888498cabde85865f3cf6dc6e0eaf5de83
kentaromiyasaka <kentamiya7426@gmail.com>
2019-10-08 11:47:28 +0800

    [Lay-out]Created Log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup',to:'users#new'
   root 'static_pages#home'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to:'sessions#new'
+  resources :sessions
 end
