b412841aa0228135aa89c171741320b68b98f31c
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-08 12:41:25 +0800

    [Function]Create Log-in (#6)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,25 @@ class SessionsController < ApplicationController
   def new
   end
     
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    # form_for sessionに入力されたemailの内容を元に、Userデータベースのカラムのデータを取得する。
+   if @user && @user.authenticate(params[:session][:password])
+    # ユーザーが存在して、passwordが一致しているかを確認するコード
+    log_in(@user)
+    flash[:success] = "Successfully logged in."
+    redirect_to user_url(@user)
+   else
+    flash.now[:danger] = "Invalid Credential"
+    render "new"
+   end
+  end
+ 
+  def destroy
+    log_out
+    flash[:info] = "Successfully logged out."
+
+    redirect_to root_url
+  end
+
 end
