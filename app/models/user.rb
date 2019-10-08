ff43719cffa16638aaad68f5cd1048416c9f1c2b
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-08 11:00:13 +0800

    [Function]Create Sign up (#4)
    * [Function]Create Sign up
    
    * added email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
+  # REGEX = Regular expression
+  # To validate correct email structure
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+ 
+  validates :name,presence:true,length:{ maximum:50 }
+  validates :email,presence:true,length:{ maximum:225 },
+              format:{with:EMAIL_REGEX},
+              uniqueness: {case_senmsitive: false }
+  has_secure_password
+  validates :password,length:{ minimum:6 },allow_nil: true
+  validates :name, presence: true
+  validates :email, presence: true
 end
