865e52c92a153d3ea188003b818f9ce14a8ddc6c
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-08 11:57:06 +0800

    [Lay-out]Created Log in (#5)
    * [Lay-out]Created Log in
    
    * I removed the route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to:'sessions#new'
+  resources :sessions
 end
