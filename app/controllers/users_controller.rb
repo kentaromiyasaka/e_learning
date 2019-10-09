7ee60b74c2f241c26e67953fa8526d57a1136b9e
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-09 11:48:41 +0800

    [Lay-out]Create show page (#8)
    * [Lay-out]Create show page
    
    * [Lay-out]Create show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
@@ -10,10 +15,6 @@ class UsersController < ApplicationController
     else
       render "new"
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
 
