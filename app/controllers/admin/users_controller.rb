0fd6232b5d271caca17a626b83955aa8b204707a
kentaromiyasaka <kentamiya7426@gmail.com>
2019-10-11 11:32:28 +0800

    [Function]Admin Function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,17 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attribute(:is_admin,true)
+    redirect_to admin_users_url
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.update_attribute(:is_admin,false)
+    redirect_to admin_users_url
+  end
+
 end
