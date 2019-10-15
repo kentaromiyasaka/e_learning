4a8def09945f3bedb737e9980b18b6d235242e14
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-15 10:29:40 +0800

    [Function]Create word (#20)

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -2,6 +2,25 @@ class Admin::WordsController < ApplicationController
   def new
     @category = Category.find(params[:category_id])
     @word = @category.words.new
+    3.times { @word.choices.new }
+    # 3回@wordのchoiceを繰り返します
+
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.new(words_params)
+    if @word.save
+      redirect_to admin_category_url(@category)
+    else
+      render "new"
+    end
+    
+  end
+
+  private
+  def words_params
+    params.require(:word).permit(:content, choices_attributes:[:id, :content, :is_correct])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_035549) do
+ActiveRecord::Schema.define(version: 2019_10_14_043419) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "choices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.boolean "is_correct"
+    t.integer "word_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
