eec44c895562f821ae7169bfc94f559561f91f75
kentaromiyasaka <kentamiya7426@gmail.com>
2019-10-17 11:16:12 +0800

    added one more dependent: :destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :words, dependent: :destroy
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
   validates :title, presence: true, length:{ maximum: 20 }
   validates :description, presence: true, length:{ maximum: 50 }
 end
