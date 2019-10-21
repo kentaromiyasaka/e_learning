a8ce5d0808a456064c05a6bf7b10ee471239d411
kentaromiyasaka <kentamiya7426@gmail.com>
2019-10-21 12:43:43 +0800

    [Function]Follow Function

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,25 @@ class User < ApplicationRecord
 
   mount_uploader :picture,PictureUploader
 
+  has_many :active_relationships, class_name: "Relationship",
+                                foreign_key: "follower_id",
+                                dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following,  through: :active_relationships, source: :followed
+  has_many :followers,  through: :passive_relationships
+
+  def follow(other_user)
+  	following << other_user
+  end
+  def unfollow(other_user)
+    active_relationships.find_by(followed: other_user).destroy
+  end
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  def relationship(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
 end
