45cb5972d3fd1099629518eaec21e112d47fc467
kentaromiyasaka <kentamiya7426@gmail.com>
2019-10-25 12:45:01 +0800

    added somethin

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,13 @@
 class WordsController < ApplicationController
   def index
-    @words = Word.all.paginate(page: params[:page], per_page: 6)
+    if params[:category]
+      @category = Category.find(params[:category])
+      @answers = @category.answers
+      @words = @answers.paginate(page: params[:page], per_page: 6)
+    else
+      @answers = current_user.answers
+      @words = @answers.paginate(page: params[:page], per_page: 6)
+    end
+    @categories = current_user.categories.left_outer_joins(:lessons).where.not(lessons:{result: nil})
   end
 end
