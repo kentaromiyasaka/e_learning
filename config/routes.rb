44c1750eec8311e2b3f2fa31b5078cd258a31665
kentaromiyasaka <54621397+kentaromiyasaka@users.noreply.github.com>
2019-10-25 12:51:12 +0800

    [Layout]words learned page (#32)
    * [Layout]words learned page
    
    * added somethin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'words/index'
   root 'static_pages#home'
   get '/signup',to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -23,7 +24,10 @@ Rails.application.routes.draw do
     end
   end
   resources :categories
+  get '/learned',to:'categories#learned'
+  get '/not_learned',to:'categories#not_learned'
   resources :lessons do
     resources :answers
   end
+  resources :words,only:[:index]
 end
